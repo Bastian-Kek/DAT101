bb4695a87acf21608d7e8c459b76957123885d30
Bastian Hansen <bastiannorbyhansen@icloud.com>
2026-09-16 08:13:49 +0200

    commit

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -3,50 +3,117 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const originalExpression = 2 + 3 * 2 - 4 * 6;
+const modifiedExpression = 2 + 3 * (2 - 4) * 6;
+
+printOut("Original expression: " + originalExpression);
+printOut("Modified expression: " + modifiedExpression);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const millimeters = 25 * 1000 + 34 * 10;
+const millPrInch = 25.4;
+const sumPart2 = millimeters / millPrInch;
+
+printOut("25 m + 34 cm = " + sumPart2.toFixed(2) + " inches");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const days = 3;
+const hours = 12;
+const minutes = 14;
+const seconds = 45;
+
+const sumPart3 = days * 24 * 60 + hours * 60 + minutes + seconds / 60;
+
+printOut("3 days, 12 hours, 14 minutes and 45 seconds = " + sumPart3 + " minutes");
 
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const totalMinutes = 6322.52;
+
+const days = Math.floor(totalMinutes / 1440);
+
+let remainder = totalMinutes % 1440;
+
+const hours = Math.floor(remainder / 60);
+remainder = remainder % 60;
+
+const minutes = Math.floor(remainder);
+const seconds = Math.round((remainder - minutes) * 60);
+
+printOut(days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds");
 
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const nok = 76;
+const usd = 8.6;
+
+const usdToNok = Math.round(54 * (nok / usd));
+const nokToUsd = Math.round(nok / (nok / usd));
+
+printOut("54 USD = " + usdToNok + " NOK");
+printOut(nok + " NOK = " + nokToUsd + " USD");
 
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const text = "There is much between heaven and earth that we do not understand.";
+
+printOut("Character count: " + text.length);
+printOut("Character at position 19: " + text.charAt(19));
+printOut("8 characters starting at position 35: " + text.substring(35, 43));
+printOut("Index of 'earth': " + text.indexOf("earth"));
 
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+printOut("5 > 3: " + (5 > 3));
+printOut("7 >= 7: " + (7 >= 7));
+printOut('"a" > "b": ' + ("a" > "b"));
+printOut('"1" < "a": ' + ("1" < "a"));
+printOut('"2500" < "abcd": ' + ("2500" < "abcd"));
+printOut('"arne" !== "thomas": ' + ("arne" !== "thomas"));
+printOut("2 === 5: " + (2 === 5));
+printOut('"abcd" > "bcd": ' + ("abcd" > "bcd"));
 
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const value1 = "254";
+const value2 = "57.23";
+const value3 = "25 kroner";
+
+printOut("Number('254'): " + Number(value1));
+printOut("parseInt('254'): " + parseInt(value1));
+printOut("parseFloat('254'): " + parseFloat(value1));
+
+printOut("Number('57.23'): " + Number(value2));
+printOut("parseInt('57.23'): " + parseInt(value2));
+printOut("parseFloat('57.23'): " + parseFloat(value2));
+
+printOut("Number('25 kroner'): " + Number(value3));
+printOut("parseInt('25 kroner'): " + parseInt(value3));
+printOut("parseFloat('25 kroner'): " + parseFloat(value3));
 
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const randomNumber = Math.floor(Math.random() * 360) + 1;
+
+printOut("Random number between 1 and 360: " + randomNumber);
 
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+const totalDays = 131;
+
+const weeks = Math.floor(totalDays / 7);
+const days = totalDays % 7;
+
+printOut(totalDays + " days = " + weeks + " weeks and " + days + " days");
 
 printOut(newLine);
